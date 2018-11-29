d52d0b3fcf5ab1b03def79b96dcc761935296137
qp29689 <qp29689@ly.com>
2018-11-29 21:06:45 +0800

    SubnetMask

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-### SubnetMAsk
+## SubnetMAsk
 
+### Introduction
+
+### Installation
+
+``` 
+npm install subnetmask
+```
+
+### Methods
+
+```
+const SubnetMask = require('subnetmask');
+
+// params: IP and Mask
+let data = SubnetMask.networkAndIpCalculator('127.0.0.1', 28);
+console.log(data);
+// { 
+//     success: true,
+//     code: '1000',
+//     data:{ 
+//         availableIpAmount: 14,
+//         mask: '255.255.255.240',
+//         network: '172.1.1.0',
+//         firstAvailable: '172.1.1.1',
+//         lastAvailable: '172.1.1.14',
+//         broadcast: '172.1.1.15' 
+//     },
+//     message: 'success'
+// }
+```
--- index.js
+++ index.js
@@ -8,24 +8,25 @@ let SubnetMask = {
      * @param {number} maskBit 掩码位 (0 - 32)
      * @returns
      */
-    networkAndIpCalculator: (ip, maskBit) => {
+    networkAndIpCalculator: function (ip, maskBit) {
         if (maskBit === +maskBit && 0 <= maskBit && maskBit <= 32) {
             let ipCheck = this.isIP(ip);
             if (ipCheck) {
                 let ipArray = ip.split( '.' );
                 let ipNum = ( ( ( ( ( ( +ipArray[ 0 ] ) * 256 ) + ( +ipArray[ 1 ] ) ) * 256 ) + ( +ipArray[ 2 ] ) ) * 256 ) + ( +ipArray[ 3 ] );
-                let mask = this.getMask(maskBit);
-                let network = this.toIpString(( ipNum & maskBit ) >>> 0);
-                let lowMask = this.getMask( 32 - maskBit );
-                let broadcast = this.toIpString(( ( ( ipNum & maskBit ) >>> 0 ) + lowMask ) >>> 0);
-                let firstAvailable = this.toIpString((( ipNum & maskBit ) >>> 0) - 1);
-                let lastAvailable = this.toIpString((( ( ( ipNum & maskBit ) >>> 0 ) + lowMask ) >>> 0) - 1);
+                let maskNum = this.getMaskNumber(maskBit);                                                                
+                let network = this.toIpString(( ipNum & maskNum ) >>> 0);
+                let lowMask = this.getLowMask( 32 - maskBit );
+                let broadcast = this.toIpString(( ( ( ipNum & maskNum ) >>> 0 ) + lowMask ) >>> 0);
+                let firstAvailable = network.substring(0, network.lastIndexOf('.')) + '.' + (Number(network.substring(network.lastIndexOf('.') + 1)) + 1);
+                let lastAvailable = broadcast.substring(0, broadcast.lastIndexOf('.')) + '.' + (Number(broadcast.substring(broadcast.lastIndexOf('.') + 1)) - 1);
+                let availableIpAmount = Number(broadcast.substring(broadcast.lastIndexOf('.') + 1)) - Number(network.substring(network.lastIndexOf('.') + 1)) - 1;
                 return {
                     success: true,
                     code: '1000',
                     data: {
-                        availableIpAmount: '',
-                        mask: mask,
+                        availableIpAmount: availableIpAmount,
+                        mask: this.toIpString(maskNum),
                         network: network,
                         firstAvailable: firstAvailable,
                         lastAvailable: lastAvailable,                        
@@ -56,7 +57,7 @@ let SubnetMask = {
      * @param {string} ip IP地址
      * @returns {boolean} 
      */
-    isIP: (ip) => {
+    isIP: function (ip) {
         if (typeof(ip) !== 'string') {
             return false;
         }
@@ -71,7 +72,7 @@ let SubnetMask = {
      *      
      * @param {number} ipNum 
      */
-    toIpString: (ipNum) => {
+    toIpString: function (ipNum) {
         var ipStr = ipNum % 256;
 		for ( let i = 3; i > 0; i-- ) {
             ipNum = Math.floor( ipNum / 256 );
@@ -81,22 +82,22 @@ let SubnetMask = {
     },
 
     /**
-     * 掩码位转换成掩码
+     * 掩码位转换成掩码数
      * @param {number} maskBit 
      */
-    getMask: (maskBit) => {
-        let mask = 0;				
+    getMaskNumber: function (maskBit) {
+        let maskNum = 0;				
 		for ( let i = 0; i < maskBit; i++ ) {
-            mask += ( 1 << ( 32 - ( i + 1 ) ) ) >>> 0;
+            maskNum += ( 1 << ( 32 - ( i + 1 ) ) ) >>> 0;
         }			        
-		return this.toIpString(mask);
+		return maskNum;
     },
 
     /**
      * 
      * @param {number} maskBitSize 掩码位(反位)
      */
-    getLowMask: (maskBitSize) => {
+    getLowMask: function (maskBitSize) {
         var lowMask = 0;			
 		for ( let i = 0; i < maskBitSize; i++ ) {
             lowMask += ( 1 << i ) >>> 0;
@@ -106,3 +107,4 @@ let SubnetMask = {
 };
 
 module.exports = SubnetMask;
+
